25fc757ff6ba392dc31e9ed3088096c9c4f7900a
yixizhixiangzhexuexi <31278862+yixizhixiangzhexuexi@users.noreply.github.com>
2017-12-12 09:50:05 +0800

    g
    g

--- src/main/java/com/lanou/controller/CollectionController.java
+++ src/main/java/com/lanou/controller/CollectionController.java
@@ -61,9 +61,10 @@ public class CollectionController {
     public void deleteCollection(HttpSession session,Integer[] goods_id,HttpServletResponse response){
         User user=(User)session.getAttribute("users");
        Integer user_id= user.getuId();
-
-              boolean result = collectionService.deleteCollection(goods_id,user_id);
-
+        boolean result = false;
+        if(collectionService.deleteCollection(goods_id,user_id)){
+                    result=true;
+                }
             FastJson_All.toJson(result,response);
 
 
